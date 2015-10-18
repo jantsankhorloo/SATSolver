40054443667ac93325a1ce382fdfcbfb268cd8e9
jantsankhorloo <jantsankhorloo@gmail.com>
2015-10-17 22:03:11 -0400

    real code

--- Main.java
+++ Main.java
@@ -7,14 +7,6 @@ public class Main {
         return null;
         
     }
-    static boolean isInt(String s) {
-        try { 
-            Integer.parseInt(s); 
-            return true; 
-        } catch(NumberFormatException er) { 
-            return false; 
-        }
-    }
     
     public static void main(String[] args) { 
         Scanner scanner = new Scanner(System.in);
@@ -22,31 +14,42 @@ public class Main {
         String expression = "";
         String finish = "done";
         
+        
         ArrayList<String> inputs = new ArrayList<String>();
+        
+        ArrayList<String> clauses = new ArrayList<String>();
+        
         
         while (!(expression = scanner.nextLine()).equals(finish)) {
             System.out.println(expression); 
             
-            inputs.add(expression);
-        }  
-        System.out.println(inputs);
-        
-        String[][] array = new String[10][1];
-        System.out.println(array);
-        int i = 0;
-        for (String input: inputs) {
-            System.out.println(input);
-            array[i][0] = input;
-            i++;
-            System.out.println(array[i][0]);
-            
-            if (!isInt(input)) {
-                //System.out.print(input);
-                //System.out.print(":");
+            for (int i = 0; i < expression.length(); i++) {
+                
+                if (Character.isAlphabetic(expression.charAt(i))) {
+                    inputs.add(expression);
+                    break;
+                }
+                if (Character.isDigit(expression.charAt(i))) {
+                    clauses.add(expression);
+                    break;
+                }
             }
             
+            
+            //inputs.add(expression);
+        }  
+        System.out.println(inputs);
+        System.out.println(clauses);
+        
+        for (String clause: clauses) {
+            
+            
         }
-        System.out.println(array);
+        
+        
+        
+        
+        scanner.close();
         
     }
     
